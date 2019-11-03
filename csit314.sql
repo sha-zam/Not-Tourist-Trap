3249a059f2354b95fba42e83d27ec4fc67b48ebb
jjs121098 <jaykepsych2000@gmail.com>
2019-11-04 01:40:36 +0800

    Third Commit

--- csit314.sql
+++ csit314.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 02, 2019 at 05:19 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.1.32
+-- Host: 127.0.0.1
+-- Generation Time: Nov 03, 2019 at 06:39 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -165,15 +165,11 @@ CREATE TABLE `spokenlanguage` (
 --
 
 INSERT INTO `spokenlanguage` (`UserID`, `LanguageID`) VALUES
-(11, 17),
-(11, 11),
-(11, 21),
-(12, 40),
-(13, 17),
-(13, 3),
-(14, 0),
-(14, 0),
-(14, 0);
+(15, 17),
+(15, 15),
+(15, 20),
+(16, 17),
+(16, 11);
 
 -- --------------------------------------------------------
 
@@ -199,7 +195,7 @@ CREATE TABLE `state` (
 --
 
 INSERT INTO `state` (`StateID`, `CountryID`, `Name`, `Description_1`, `Description_2`, `Image_1`, `Image_2`, `Image_3`, `Title_1`, `Title_2`) VALUES
-(7, 2, 'Paris', 'The Champs-Élysées is a truly lovely avenue: a picture postcard scene. Nearly 2 kilometres in length, this historic thoroughfare runs from Place de la Concorde to the majestic Arc de Triomphe. But though it has since become ‘the world’s most beautiful avenue’, the Champs-Élysées was once a swamp. The avenue has only become more beautiful with every passing decade.', 'Set in the heart of Paris, the former French royal palace today is home to one of the largest and most renowned art collections in the world. Home to Leonardo da Vinci\'s Mona Lisa, the Louvre is considered the world\'s greatest art museum, with an unparalleled collection of items covering the full spectrum of art through the ages', 'champs_elysees.jpg', 'louvre.jpg', 'paris.jpg', 'The Champs-Élysées, past and present', 'The Musée du Louvre'),
+(7, 2, 'Paris', 'The Champs-Elysees is a truly lovely avenue: a picture postcard scene. Nearly 2 kilometres in length, this historic thoroughfare runs from Place de la Concorde to the majestic Arc de Triomphe. But though it has since become the world\'s most beautiful avenue, the Champs-Elysees was once a swamp. The avenue has only become more beautiful with every passing decade.', 'Set in the heart of Paris, the former French royal palace today is home to one of the largest and most renowned art collections in the world. Home to Leonardo da Vinci\'s Mona Lisa, the Louvre is considered the world\'s greatest art museum, with an unparalleled collection of items covering the full spectrum of art through the ages', 'champs_elysees.jpg', 'louvre.jpg', 'paris.jpg', 'The Champs-Elysees, past and present', 'The Musee du Louvre'),
 (9, 9, 'Prague', 'Prague Castle is a castle complex in Prague, Czech Republic, built in the 9th century. It is the official office of the President of the Czech Republic. The castle was a seat of power for kings of Bohemia, Holy Roman emperors, and presidents of Czechoslovakia.', 'Connecting the Old town with Lesser Town, this popular pedestrian bridge is filled with musicians, painters, vendors and tourists during the summertime.', 'prague_castle.jpg', 'Charles_bridge.jpg', 'prague.jpg', 'Prague Castle', 'Charles Bridge');
 
 -- --------------------------------------------------------
@@ -220,6 +216,13 @@ CREATE TABLE `tour` (
   `Price` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tour`
+--
+
+INSERT INTO `tour` (`TourID`, `Name`, `Description`, `TourGuideID`, `CountryID`, `StateID`, `Start_date`, `End_date`, `Price`) VALUES
+(1, 'Love is in the Air!', 'Romantissimo', 16, 2, 7, '2019-11-03', '2019-11-06', '300');
+
 -- --------------------------------------------------------
 
 --
@@ -232,6 +235,14 @@ CREATE TABLE `tourimage` (
   `AddedByUser` int(11) NOT NULL,
   `Image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tourimage`
+--
+
+INSERT INTO `tourimage` (`TourImgID`, `TourID`, `AddedByUser`, `Image`) VALUES
+(1, 1, 16, '1572778650_Chez-Paul-Paris-Alamy.jpg'),
+(2, 1, 16, '1572778650_untitled.png');
 
 -- --------------------------------------------------------
 
@@ -259,19 +270,17 @@ CREATE TABLE `user` (
   `FirstName` varchar(255) NOT NULL,
   `LastName` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
-  `Password` text NOT NULL
+  `Password` text NOT NULL,
+  `Profile_Image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`UserID`, `FirstName`, `LastName`, `Email`, `Password`) VALUES
-(1, 'Jonathan', 'Lua', 'jonlua@gmail.com', 'password'),
-(11, 'System', 'Admin', 'sysadmin@gmail.com', 'sysadmin123'),
-(12, 'Huzair', 'Yazid', 'huzairyazid@gmail.com', 'sysadmin123'),
-(13, 'zayn', 'malik', 'Pu55yslayer@gmail.com', 'abcd1234!'),
-(14, 'Pu55y', 'Slayer', 'abcd@gmail.com', '12345678');
+INSERT INTO `user` (`UserID`, `FirstName`, `LastName`, `Email`, `Password`, `Profile_Image`) VALUES
+(15, 'System', 'Admin', 'system@gmail.com', 'a9a9e9dcb45962970eac445259464b3f', ''),
+(16, 'Russell', 'Leong', 'rleong@gmail.com', 'af96cfbbb950a4123e0d08cfac0baf55', '1572777080_y.jpeg');
 
 --
 -- Indexes for dumped tables
@@ -351,13 +360,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `BookingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `BookingID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `country`
 --
 ALTER TABLE `country`
-  MODIFY `CountryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `CountryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `language`
@@ -369,19 +378,19 @@ ALTER TABLE `language`
 -- AUTO_INCREMENT for table `state`
 --
 ALTER TABLE `state`
-  MODIFY `StateID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `StateID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tour`
 --
 ALTER TABLE `tour`
-  MODIFY `TourID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `TourID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tourimage`
 --
 ALTER TABLE `tourimage`
-  MODIFY `TourImgID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `TourImgID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tourreview`
@@ -393,7 +402,7 @@ ALTER TABLE `tourreview`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `UserID` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
